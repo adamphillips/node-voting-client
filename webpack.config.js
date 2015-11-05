f69194af40b764ded1e32a506d30d96e26b64ea5
Adam Phillips <aphillips@scholastic.co.uk>
2015-11-05 23:05:01 +0000

    Enabled react hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
+
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
+
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
