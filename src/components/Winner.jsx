e5ff69a519cf118d6ca33627fa790642dcaf38e7
Adam Phillips <aphillips@scholastic.co.uk>
2015-11-07 00:31:39 +0000

    Improved display of results

--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import classNames from 'classnames'
+import {classnameForEntry} from '../utilities';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
 
   render: function() {
     return <div className="winner">
-      Winner is {this.props.winner}!
+      <div className={classNames('container', classnameForEntry(this.props.winner))}>
+        <h1>{this.props.winner} wins!</h1>
+      </div>
     </div>;
   }
 });
